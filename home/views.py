88df96f1c73fcd7c594ea33d3c2708f8240f2877
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-09-20 10:30:57 +0600

    Fixed views.py error

--- home/views.py
+++ home/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'index/index.html')
 
+
 def products(request):
     return render(request, 'products/all_product.html')
+
+
+def customers(request):
+    return render(request, 'review/customer_review.html')
+
+
+def vendors(request):
+    return render(request, 'review/vendor_review.html')
+
+
+def contact(request):
+    return render(request, 'others/contact.html')
