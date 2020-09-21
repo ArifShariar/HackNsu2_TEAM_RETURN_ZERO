8a0786af65b1da2fd8740954d36db07dc165ccbd
Arif Shariar Rahman <43639863+1705095@users.noreply.github.com>
2020-09-21 12:08:55 +0600

    Fixed Order View

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from .forms import orderForm
 from products import models as p
 import datetime
 
+
 # Create your views here.
 
 def check_usertype(request):
@@ -24,6 +25,7 @@ def check_usertype(request):
     else:
         return ' ', ' '
 
+
 def index(request):
     usertype, _ = check_usertype(request)
 
@@ -36,7 +38,6 @@ def index(request):
 
 
 def products(request):
-
     dict = {}
     dict['raw_materials'] = False
 
@@ -51,55 +52,42 @@ def products(request):
 
 def place_order(product_obj, amount, customer_obj):
     time = datetime.date.today()
-    order = p.order.objects.create(order_time=time, order_amount=amount, customer_fk=customer_obj, product_fk=product_obj)
+    order = p.order.objects.create(order_time=time, order_amount=amount, customer_fk=customer_obj,
+                                   product_fk=product_obj)
     order.save();
 
-def order_view(request , pk):
+
+def order_view(request, pk):
     if request.method == 'POST':
         amount = request.POST.get('amount')
-         #amount= Int(amount)
-        prod = p.company_product.objects.get(pk = pk)
+        # amount= Int(amount)
+        prod = p.company_product.objects.get(pk=pk)
         customer = list(ls.Customer.objects.filter(user=request.user))[0]
-        place_order(prod , amount , customer)
+        place_order(prod, amount, customer)
         print("orderplaced")
-        #print(amount)
+        # print(amount)
     dict = {}
-    pob = p.company_product.objects.get(pk = pk)
-    dict['product_name'] = pob.name
-    dict['price'] = pob.price
-    dict['stock'] = pob.stock
-    dict['form'] = orderForm
-    dict['raw_materials'] = False
-    usertype,_ = check_usertype(request)
-
-    if usertype.lower() == 'vendor' or usertype.lower()=='admin' or usertype.lower()=='employee':
-        dict['raw_materials'] = True
-    return render(request, 'order/order.html', dict)
-
-
-    dict = {}
-    pob = p.company_product.objects.get(pk = pk)
+    pob = p.company_product.objects.get(pk=pk)
     dict['product_name'] = pob.name
     dict['price'] = pob.price
     dict['stock'] = pob.stock
     dict['form'] = orderForm
     dict['raw_materials'] = False
     usertype, _ = check_usertype(request)
+
     if usertype.lower() == 'vendor' or usertype.lower() == 'admin' or usertype.lower() == 'employee':
         dict['raw_materials'] = True
-    return render(request, 'order/order.html' , dict)
-
-
+    return render(request, 'order/order.html', dict)
 
 
 def customer_order_history_view(request):
     cust_obj = list(ls.Customer.objects.filter(user=request.user))[0]
-    #print(cust_obj)
-    orders = list(p.order.objects.filter(customer_fk = cust_obj))
-    dict={}
+    # print(cust_obj)
+    orders = list(p.order.objects.filter(customer_fk=cust_obj))
+    dict = {}
     dict['order_list'] = orders
     dict['customer_name'] = cust_obj
-    return render(request, 'order/customer_order_history.html' , dict)
+    return render(request, 'order/customer_order_history.html', dict)
 
 
 def customers(request):
@@ -119,7 +107,7 @@ def vendors(request):
     vendors_raw = Vendor.objects.all()
     for v in vendors_raw:
         vendors.append([v.company_name, v.user.email, v.user.id])
-    dict = {'vendors':vendors}
+    dict = {'vendors': vendors}
 
     dict['raw_materials'] = False
     usertype, _ = check_usertype(request)
@@ -130,7 +118,6 @@ def vendors(request):
 
 
 def contact(request):
-
     dict = {}
 
     dict['raw_materials'] = False
@@ -140,8 +127,8 @@ def contact(request):
 
     return render(request, 'others/contact.html', dict)
 
-def vendor_public_profile(request, vendor_user_id):
 
+def vendor_public_profile(request, vendor_user_id):
     user = User.objects.get(id=vendor_user_id)
     vendor = Vendor.objects.get(user=user)
 
